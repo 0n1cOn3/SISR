11a22758937e17bb3452710343a30feba1bfcb87
0n1cOn3 <0n1cOn3@gmx.ch>
2019-10-17 16:31:54 +0200

    Update megascript.sh

--- megascript.sh
+++ megascript.sh
@@ -11,12 +11,12 @@ echo ""
 selection=
 until [ "$selection" = "0"]; do
 	clear
-	 echo "               Shodan IP Scan Resulter V0.1.1                 "
+	 echo "               Shodan IP Scan Resulter V0.1.1               "
 	 echo "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
 	 echo "    Made by 0n1cOn3                                         "
 	 echo "     	Script only for                                    "
-	 echo "				   test purposes                   "
-	 echo "     		        !Automated Script!                 "
+	 echo "				   test purposes                   			   "
+	 echo "     		        !Automated Script!                 	   "
 	 echo ""
      echo "Setup Menu:"
      echo ""
@@ -79,14 +79,63 @@ echo   Github: https://github.com/0n1cOn3/ShodanIPScanResulter
 echo   Whatsapp: https://cutt.ly/meaORzg 
 echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
               read -p " Press any Key ... ";;
-         1 ) echo                Shodan IP Scan Resulter V0.1.1
+         1 )
 echo                Shodan IP Scan Resulter V0.1.1
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+echo      Made by 0n1cOn3
+echo      		Script only for
+echo				test purposes
+echo      		!Automated Script!
+echo ""
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+sleep 2
+clear
+echo                Shodan IP Scan Resulter V0.1.1
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+echo Lets grab some IPs from Shodan.io
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+				sleep 0.5
+#				echo "PASSWORD" |python search.py search apache >> ip.txt | say loading, please wait
+				python search.py search apache >> ip.txt | say loading, please wait.
+				clear
+				sleep 0.4
+	echo                Shodan IP Scan Resulter V0.1.1
+	echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+	echo IPs loaded, lets start scan! This will take really some time! Be Patience!
+	sleep 1
+	echo This Script is running in background.
+	sleep 2
+	echo Might take a while till the screen continues.
+	sleep 3
+	sudo -S python search.py scan ip-temp.txt
+	clear
+		sleep 0.1
+echo                Shodan IP Scan Resulter V0.1.1
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+echo Scan finished!
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+sleep 2
+clear
+echo                Shodan IP Scan Resulter V0.1.1
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+echo The result will be loaded into nano. Just a second please.
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+sleep 1
+nano results.txt
+sleep 0.5
+clear
+echo                Shodan IP Scan Resulter V0.1.1
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+echo   Github: https://github.com/0n1cOn3/ShodanIPScanResulter
+echo   Whatsapp: https://cutt.ly/meaORzg 
+echo ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
             exit;;
          3 ) echo "Windows is currently not yet supported. Stay tuned!"
                 exit;;
+		 5 ) 
 		 4 ) echo "nano results.txt"
 		 		exit;;
 				0 ) exit;;
-         * ) echo "Please enter 1,2,3,4 or 0"
+         * ) echo "Please enter 1,2,3,4,5 or 0"
      esac
 done
