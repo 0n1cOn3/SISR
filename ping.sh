790806ee365d4e70acad74fa86d3314b09bf0db4
TheSpeedX <ggspeedx29@gmail.com>
2019-10-17 19:29:13 +0530

    Fixed Again

--- ping.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat ip.txt | while read line
-do
-sudo -A --askpass nmap -sS $line
-done
--- search.py
+++ search.py
@@ -1,16 +1,24 @@
+import sys,os
 import shodan
 
-SHODAN_API_KEY = "****************************"
-
-api = shodan.Shodan(SHODAN_API_KEY)
-
-try:
-
-	# Seach Shodan
-	results = api.search('apache')
-
-	# Show the result
-	for result in results['matches']:
-		print '%s' % result['ip_str']
-except shodan.APIError, e:
-	print 'Error: %s' % e
+if len(sys.argv)!=3:
+	print('Wrong Usage')
+if sys.argv[1].lower()=='search':
+	SHODAN_API_KEY = "SHODAN_API_KEY_GOES_HERE"
+	api = shodan.Shodan(SHODAN_API_KEY)
+	try:
+		results = api.search(sys.argv[2])
+		# Show the result
+		f=open('ip-temp.txt','w')
+		for result in results['matches']:
+			ip=result['ip_str']
+			print('Found IP: ',ip)
+			f.write(ip+'\n')
+		f.close()
+	except:
+		print('Error Occurred')
+elif sys.argv[1].lower()=='scan':
+	f=open(sys.argv[2],'r').readlines()
+	for i in range(len(f)):
+		print('Scanning '+str(i+1)+' out of '+str(len(f))+' IPs')
+		os.system('nmap -sS '+f[i]+' >>results.txt')
