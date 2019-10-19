4cdc1850fb9ecc9e6345413c0523429cea57a32f
0n1cOn3 <0n1cOn3@gmx.ch>
2019-10-20 00:19:08 +0200

    Update update.py

--- update.py
+++ update.py
@@ -1,2 +1,2 @@
 import os
-os.system("clear && cd .. && rm -rf Shodan-IP-Scan-Resulter/ &&  git clone https://github.com/0n1cOn3/Shodan-IP-Scan-Resulter && cd Shodan-IP-Scan-Resulter/ && chmod +x megascript.sh && clear &&figlet Update finished && sleep 2 && echo Script will be restarted now && sleep 3 && ./megascript.sh")
+os.system("clear && cd .. && rm -rf Shodan-IP-Scan-Resulter/ &&  git clone https://github.com/0n1cOn3/Shodan-IP-Scan-Resulter && cd Shodan-IP-Scan-Resulter/ && chmod +x megascript.sh && clear &&figlet Update finished && sleep 3 && echo Script will be restarted now && sleep 5 && ./megascript.sh")
