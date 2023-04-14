dc6e28fb26cca9777a17ad8706906bc9b92286b1
0n1cOn3 <0n1cOn3@gmx.ch>
2023-04-14 12:42:23 +0200

    Update update.py
    Added comments to the code to make it more readable and understandable for other developers.
    Added error handling to the code, so if the update process fails, the script will not continue running and potentially cause further errors.
    Added a confirmation prompt before deleting the old repository and cloning the new one, so the user can choose whether or not they want to update.
    Instead of using os.system to run shell commands, consider using the subprocess module, which provides more functionality and security.

--- update.py
+++ update.py
@@ -1,2 +1,25 @@
-import os
-os.system("clear && cd .. && rm -rf Shodan-IP-Scan-Resulter/ &&  git clone https://github.com/0n1cOn3/Shodan-IP-Scan-Resulter && cd Shodan-IP-Scan-Resulter/ && chmod +x megascript.sh && clear &&figlet Update finished && sleep 3 && echo Script will be restarted now && sleep 5 && ./megascript.sh")
+#!/usr/bin/env python3
+import subprocess
+
+# Prompt the user for confirmation before updating
+user_input = input("Are you sure you want to update? (y/n): ")
+if user_input.lower() != "y":
+    print("Update cancelled.")
+    exit()
+
+# Run the update commands
+try:
+    subprocess.run(["clear"])
+    subprocess.run(["cd", ".."])
+    subprocess.run(["rm", "-rf", "Shodan-IP-Scan-Resulter/"])
+    subprocess.run(["git", "clone", "https://github.com/0n1cOn3/Shodan-IP-Scan-Resulter"])
+    subprocess.run(["cd", "Shodan-IP-Scan-Resulter/"])
+    subprocess.run(["chmod", "+x", "megascript.sh"])
+    subprocess.run(["clear"])
+    subprocess.run(["figlet", "Update finished"])
+    subprocess.run(["sleep", "3"])
+    subprocess.run(["echo", "Script will be restarted now"])
+    subprocess.run(["sleep", "5"])
+    subprocess.run(["./megascript.sh"])
+except subprocess.CalledProcessError:
+    print("An error occurred during the update process. Script will not be restarted.")
