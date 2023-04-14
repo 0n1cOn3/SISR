b34c476eeaa1038236766a2fdc9a563f12d2218f
0n1cOn3 <0n1cOn3@gmx.ch>
2023-04-14 12:34:03 +0200

    Simplified the Update Script

--- update-engine.sh
+++ update-engine.sh
@@ -1,83 +1,58 @@
 #!/bin/bash
-selection=
-until [ "$selection" = "0"]; do
-	clear
-	echo  "  Shodan IP Scan Resulter Core Updater  "
-	echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	echo ""
-	echo "1 - Linux"
-	echo "2 - Termux"
-	echo "3 - macOS"
-	echo "4 - Windows (Not Supported yet)"
-	echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-    echo -n "Enter Selection:"
-    read selection
-    echo ""
-    case $selection in
-	1 )
-	clear
-	echo  "  Shodan IP Scan Resulter Core Updater  "
-	echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	echo  "	        Install Requirements           "
-	echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	sudo -S apt install python3 python3-pip
-	echo  "  Shodan IP Scan Resulter Core Updater  "
-	echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	echo  "				   Finish                  "     
-	echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	sleep 2
-	./megascript
-	exit;;
-		2 )
-		clear
-		echo  "  Shodan IP Scan Resulter Core Updater  "
-		echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-		echo  "	        Install Requirements           "
-		echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-		apk install python3 python3-pip
-		echo  "  Shodan IP Scan Resulter Core Updater  "
-		echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-		echo  "				   Finish                  "
-		echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-		sleep 2
-		./megascript
-		exit;;
-			3 )
-			clear
-			echo  "  Shodan IP Scan Resulter Core Updater  "
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			echo  "Install Brew                "
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-			clear
-			echo  "  Shodan IP Scan Resulter Core Updater  "
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			echo  "Install Requirements           "
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			brew install python3
-			pip install shodan
-			clear
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			echo  "Almost done"
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			sleep 3
-			clear
-			echo  "  Shodan IP Scan Resulter Core Updater  "
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			echo  "Finish                  "
-			echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-			sleep 3
-			./megascript.sh
-		    exit;;
-				4 )
-				clear
-				echo  "  Shodan IP Scan Resulter Core Updater  "
-				echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-				echo  "Not yet supported. Sorry :(       "
-				echo  "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-				sleep 5
-				exit;;
-					0 ) exit;;
-	            		* ) echo "Please enter 1,2,3 or 0 for exit"
-	  esac
+while [[ "$selection" != "0" ]]; do
+clear
+echo "Shodan IP Scan Resulter Core Updater"
+echo "==================================="
+echo ""
+echo "1 - Linux"
+echo "2 - Termux"
+echo "3 - macOS"
+echo "4 - Windows (Not Supported yet)"
+echo "==================================="
+read -p "Enter selection: " selection
+echo ""
+case $selection in
+1 )
+  clear
+  echo "Shodan IP Scan Resulter Core Updater"
+  echo "==================================="
+  echo "Install Requirements"
+  echo "==================================="
+  sudo apt install -y python3 python3-pip
+  echo "Shodan IP Scan Resulter Core Updater"
+  echo "==================================="
+  echo "Finish"
+  echo "==================================="
+  sleep 2
+  ./megascript
+exit;;
+2 )
+  clear
+  echo "Shodan IP Scan Resulter Core Updater"
+  echo "==================================="
+  echo "Install Requirements"
+  echo "==================================="
+  apk add --no-cache python3 py3-pip
+  echo "Shodan IP Scan Resulter Core Updater"
+  echo "==================================="
+  echo "Finish"
+  echo "==================================="
+  sleep 2
+  ./megascript
+  exit;;
+3 )
+  clear
+  echo "Shodan IP Scan Resulter Core Updater"
+  echo "==================================="
+  echo "Install Requirements"
+  echo "==================================="
+  brew install python3
+  echo "Shodan IP Scan Resulter Core Updater"
+  echo "==================================="
+  echo "Finish"
+  echo "==================================="
+  sleep 2
+  ./megascript
+  exit;;
+ esac
 done
