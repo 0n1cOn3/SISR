97efe25fb43def4c927523ee8b0071e1a125e3d7
0n1cOn3 <0n1cOn3@gmx.ch>
2023-10-21 16:16:02 +0200

    Simplified commands and script structure

--- megascript.sh
+++ megascript.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
-selection=
+while true; do
+    clear
+    echo "=============================="
+    echo "Shodan IP Scan Resulter V1.0RC"
+    echo "=============================="
+    echo "Made with <3 by 0n1cOn3"
+    echo "Script only for test purposes"
+    echo "=============================="
+    echo "Github: https://github.com/0n1cOn3/ShodanIPScanResulter"
+    echo "=============================="
 
-until [ "$selection" = "0" ]; do
-	clear
-	echo "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	echo "               Shodan IP Scan Resulter V1.0RC              "
-	echo "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	echo "Made with <3 by 0n1cOn3"
-	echo "Script only for test purposes"
-	echo "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	echo "Github: https://github.com/0n1cOn3/ShodanIPScanResulter
-	echo "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+    echo "Setup Menu:"
+    echo "1 - Search Apache"
+    echo "2 - Scan IPs"
+    echo "0 - Exit"
 
-	echo "Setup Menu:"
-	echo "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
-	echo "1 - Search Apache"
-	echo "2 - Scan IPs"
-	echo "0 - Exit"
-	echo "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+    read -p "Enter your choice: " selection
+    echo ""
 
-	echo -n "Enter your choice: "
-	read selection
-	echo ""
+    case $selection in
+        1)
+            python search.py search apache >> ip.txt
+            echo "Loading, please wait..."
+            sleep 5
+            ;;
+        2)
+            # Replace "sudo" if not necessary
+            sudo -S python search.py scan ip-temp.txt
+            ;;
+        0)
+            echo "Exiting..."
+            exit
+            ;;
+        *)
+            echo "Invalid choice, please try again."
+            ;;
+    esac
 
-	case $selection in
-		1)
-			python search.py search apache >> ip.txt && say "Loading, please wait" && sleep 5
-			;;
-		2)
-			# Replace "sudo" if not necessary
-			sudo -S python search.py scan ip-temp.txt
-			;;
-		0)
-			echo "Exiting..."
-			;;
-		*)
-			echo "Invalid choice, please try again."
-			;;
-	esac
-
-	echo ""
-	read -n 1 -s -r -p "Press any key to continue..."
+    read -n 1 -s -r -p "Press any key to continue..."
 done
